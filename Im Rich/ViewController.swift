887543f607eeadb32ca9a640257c9ade50d929d3
Dzaky <68675197+walkinglele@users.noreply.github.com>
2020-12-09 10:31:21 +0700

    Add files via upload

---
+++ Im Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Im Rich
+//
+//  Created by Dzaky Saputra on 09/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
